611d97059c7a567312cfd0d2c2df8797a4fac81d
hellwind2019 <daniltorianik2016@gmail.com>
2025-08-18 12:03:58 +0300

    Add refs to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,33 @@
+import { useRef, type FormEvent } from "react";
 import { Button } from "./ui/button";
 import { Card, CardAction, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) {
+      console.log(nameRef.current.value);
+    }
+  };
   return (
     <Card className="max-w-md w-full gap-sm rounded-xl shadow-lg border border-gray-400 bg-neutral-300">
       <CardContent>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log("Sumbit");
-          }}
-        >
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <Label htmlFor="name" className="pb-3">
               Name
             </Label>
-            <Input id="name" type="text" />
+            <Input ref={nameRef} id="name" type="text" />
           </div>
           <div className="mb-3">
             <Label htmlFor="age" className="pb-3">
               Age
             </Label>
-            <Input id="age" type="number" />
+            <Input ref={ageRef} id="age" type="number" />
           </div>
           <CardAction>
             <Button type="submit">Submit</Button>
